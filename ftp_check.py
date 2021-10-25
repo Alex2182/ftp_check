d0863927444d58e9676cea54f4efcaaf9b2f9564
Alex2182 <alex21.82@mail.ru>
2021-10-25 13:09:33 +0000

    FTP 1.0

---
+++ ftp_check.py
@@ -0,0 +1,30 @@
+import ftplib
+import telegram_send
+import datetime 
+
+
+ftp = ftplib.FTP("kscftpserv.ksc.local")
+ftp.login("1csynctvzkscuser", "Gb43#gbw2")
+
+ftp.cwd('TVZ_KCK')
+files = []
+with open("file_list.txt", "r") as myfile:
+    file_list =myfile.read() 
+date = datetime.datetime.now().strftime("%y%m%d")
+#date = '211022'
+#print(date)
+try:
+    files = ftp.nlst()
+except ftplib.error_perm as resp:
+    if str(resp) == "550 No files found":
+        print ("No files in this directory")
+    else:
+        raise
+
+for f in files:
+    if (f.find(date)>=0 and file_list.find(f)<0):
+       with open("file_list.txt", "a") as myfile:
+           myfile.write(f)
+       text = [f'Найден новый файл {f}']
+       telegram_send.send(messages=text)
+       # print (f)
